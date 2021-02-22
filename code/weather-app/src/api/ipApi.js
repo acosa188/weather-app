1511e3fab4b992f6d39a9f55550c484a9640a9c6
Arjun Cosare <arjun.cosare@outlook.com>
2021-02-21 23:32:12 -0700

    updating api

--- code/weather-app/src/api/ipApi.js
+++ code/weather-app/src/api/ipApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getLocation(callback){
     await axios
-        .get(`http://api.ipapi.com/api/check?access_key=${process.env.VUE_APP_IPAPI_KEY}`)
+        .get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.VUE_APP_IPAPI_KEY}`)
         .then((r) => {
             callback(r.data);
         })
--- code/weather-app/src/api/weatherApi.js
+++ code/weather-app/src/api/weatherApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getWeather(city, callback){
     await axios
-        .get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.VUE_APP_WEATHER_KEY}&units=metric`)
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.VUE_APP_WEATHER_KEY}&units=metric`)
         .then((r) => {
             callback(r.data);
         })
